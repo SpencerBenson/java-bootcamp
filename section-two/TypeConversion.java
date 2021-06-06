0f760ac581ce19508891559185fe849c27be9ca4
Spencer Benson <bennspencer@gmail.com>
2021-06-06 15:43:35 +0300

    add type casting.

--- section-two/TypeConversion.java
+++ section-two/TypeConversion.java
@@ -1,5 +1,10 @@
-package section-two;
 
 public class TypeConversion {
+  public static void main(String[] args){
+    double salary = 5833.33333;
+    int roundedSalary = (int) salary;
+    System.out.println("Spencer's monthly salary is $" + roundedSalary);
+
+  }
   
 }
