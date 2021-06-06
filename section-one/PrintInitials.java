d65d4382203d85b57c13439317b42f3748219c4e
Spencer Benson <bennspencer@gmail.com>
2021-06-06 14:31:35 +0300

    print initials

---
+++ section-one/PrintInitials.java
@@ -0,0 +1,12 @@
+public class PrintInitials{
+  public static void main(String[] args) {
+    System.out.println(" SSS      PPPP ");
+    System.out.println("S   S     P   P");
+    System.out.println("S         P   P");
+    System.out.println(" SSS      PPPP");
+    // System.out.println("    S     P");
+    System.out.println("    S     P");
+    System.out.println("S   S     P");
+    System.out.println(" SSS      P");
+  }
+}
