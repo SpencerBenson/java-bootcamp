1f07af95268e6e1b47c1623f7a7212368cb8f3f9
Spencer Benson <bennspencer@gmail.com>
2021-06-06 18:26:02 +0300

    add bool comparison

---
+++ Booleana.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Booleana {
+  public static void main(String[] args){
+    int chemistryGrade = 95;
+    int biologyGrade = 75;
+    System.out.println(chemistryGrade < biologyGrade);
+
+    String sentence = "I love this course!";
+    String sentence2 = "I love this course!";
+    System.out.println(sentence.equals(sentence2));
+    System.out.println(!sentence.equals(sentence2));
+  }
+  
+}
