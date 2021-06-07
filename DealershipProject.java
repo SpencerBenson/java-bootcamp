d7a22cffb4754dd9bbb594f3b85e150aa29a0796
Spencer Benson <bennspencer@gmail.com>
2021-06-07 11:01:36 +0300

    add logic for buying a car.

---
+++ DealershipProject.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class DealershipProject {
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    
+
+    System.out.println("Welcome to KAYAD Dealership!");
+    System.out.println("\nWould you like to:");
+    System.out.println("'a'• Buy a car ");
+    System.out.println("'b'• Sell a car ");
+    String option = scan.nextLine();
+
+    switch(option){
+      case "a":
+        System.out.println("What is your budget?");
+        int budget = scan.nextInt();
+        if(budget >= 10000){
+          System.out.println("Great, a nissan Altima is available.");
+          System.out.println("\nDo you have insurance? Write 'yes' or 'no'.");
+          scan.nextLine();
+          String hasInsurance = scan.nextLine();
+          
+          System.out.println("\nDo you have a driver's licence? Write 'Yes' or 'no'.");
+          String hasDL = scan.nextLine();
+
+          System.out.println("\nWhat is your creditscore?");
+          int creditScore = scan.nextInt();
+
+          if(hasInsurance.equals("yes") && creditScore >= 660 && hasDL.equals("yes")){
+            System.out.println("Deal! The car is yours. Pleasure doing business with you.");
+          }else{
+            System.out.println("We're Sorry! You are not elligible.");
+          }
+
+        }else{
+          System.out.println("We don't sell cars under $10000. Sorry!");
+        }
+      break;
+      case "b":
+      
+      break;
+      default:
+
+    }
+    scan.close();
+
+  }
+  
+}
